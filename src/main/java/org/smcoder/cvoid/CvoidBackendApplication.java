a2c41ac2eb43a5a394bb68d5a4a0066654402a5c
冯志勇 <smcoder@fengzhiyongs-MacBook-Pro.local>
2022-04-14 17:14:59 +0800

    feature:初始化

---
+++ src/main/java/org/smcoder/cvoid/CvoidBackendApplication.java
@@ -0,0 +1,13 @@
+package org.smcoder.cvoid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvoidBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CvoidBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/smcoder/cvoid/CvoidBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.smcoder.cvoid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvoidBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
