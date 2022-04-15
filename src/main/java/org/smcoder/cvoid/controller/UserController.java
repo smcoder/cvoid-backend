9c85e34d136fce70e421450a8dfb393ec01503b1
冯志勇 <smcoder@fengzhiyongs-MacBook-Pro.local>
2022-04-15 18:45:38 +0800

    feature:code

--- src/main/java/org/smcoder/cvoid/controller/UserController.java
+++ src/main/java/org/smcoder/cvoid/controller/UserController.java
@@ -1,7 +1,21 @@
 package org.smcoder.cvoid.controller;
 
+import org.smcoder.cvoid.domain.SmUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class UserController {
+
+    public Object register(@RequestBody SmUser user) {
+        return null;
+    }
+
+    public Object login(@RequestBody SmUser user) {
+        return null;
+    }
+
+    public Object logout() {
+        return null;
+    }
 }
